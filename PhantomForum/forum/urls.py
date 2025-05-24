810d6049ba5f1c5ce1ef1302d562f3effbc0f900
Maxim <maxsavikin@gmail.com>
2025-05-24 15:45:51 +0300

    Добавил просмотр всех заказов

--- PhantomForum/forum/urls.py
+++ PhantomForum/forum/urls.py
@@ -6,5 +6,6 @@ app_name = "forum"
 urlpatterns = [
     path("", views.AboutView.as_view(), name="about"),
     path("create-target", views.AddRequestView.as_view(), name="create"),
-    path("<int:pk>/", views.RequestView.as_view(), name="request")
+    path("<int:pk>/", views.RequestView.as_view(), name="request"),
+    path("targets/", views.ListRequestsView.as_view(), name='targets'),
 ]
--- PhantomForum/forum/views.py
+++ PhantomForum/forum/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, DetailView, CreateView
+from django.views.generic import TemplateView, DetailView, CreateView, ListView
 from .models import Request
 from .forms import AddRequest
 from django.urls import reverse_lazy
@@ -11,6 +11,13 @@ class AboutView(TemplateView):
 class RequestView(DetailView):
     model = Request
     template_name = "forum/request_view.html"
+
+class ListRequestsView(ListView):
+    template_name = "forum/list_requests.html"
+    context_object_name = "requests"
+
+    def get_queryset(self):
+        return Request.objects.all()
 
 class AddRequestView(CreateView):
     template_name = "forum/add_request.html"
